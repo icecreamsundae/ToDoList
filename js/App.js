a7fb30d307ae7ef64c2ea56629685728796a7444
icecreamsundae <eunjeongkeem@gmail.com>
2022-06-24 16:33:04 +0900

    시간 딜레이 해결

--- js/App.js
+++ js/App.js
@@ -11,6 +11,6 @@ function currentTime() {
 
 }
 
-setInterval(currentTime, 1000);
+currentTime(); // 먼저 함수 실행을 해서 시간을 보여주고 (먼저 해줘야 새로고침해도 1초 딜레이없이 바로 보인다)
+setInterval(currentTime, 1000); // 그 다음에 1초씩 보여줌
 
-// 해결할 것 : 새로고침 시 00:00:00 머물다가 시간 보여줌.. 
